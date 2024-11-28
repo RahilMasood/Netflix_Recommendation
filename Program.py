afbe2d43e5eaefc541e916ee94b33eecfea2f7e9
Rahil <145386395+RahilMasood@users.noreply.github.com>
2024-11-28 12:14:11 +0530

    Update Program.py

--- Program.py
+++ Program.py
@@ -2,211 +2,45 @@ import string
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import plotly.graph_objects as go
-import plotly.express as px
 from wordcloud import WordCloud
 import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-import numpy as np
-from scipy.sparse import save_npz
-import warnings
 import re
-warnings.filterwarnings('ignore')
 
+# Read data
 netflix_data = pd.read_csv(r"C:\Users\shez8\Desktop\RAHIL\Mini Projects\Movie Recommender\netflix_data.csv")
-netflix_data.head()
-
-
 netflix_data.fillna('', inplace=True)
 
+# Preprocess data
+netflix_data['description'] = netflix_data['description'].apply(lambda x: x.translate(str.maketrans('', '', string.punctuation)).lower())
 
-movie_counts = netflix_data['release_year'].value_counts().sort_index()
-'''fig = go.Figure(data=go.Bar(x=movie_counts.index, y=movie_counts.values))
-fig.update_layout(
-    plot_bgcolor='rgb(17, 17, 17)',  
-    paper_bgcolor='rgb(17, 17, 17)',  
-    font_color='white', 
-    title='Number of Movies Released Each Year',  
-    xaxis=dict(title='Year'),  
-    yaxis=dict(title='Number of Movies')
-)
-fig.update_traces(marker_color='red')
-fig.show()'''
-
-
-movie_type_counts = netflix_data['type'].value_counts()
-
-'''fig = go.Figure(data=go.Pie(labels=movie_type_counts.index, values=movie_type_counts.values))
-
-fig.update_layout(
-    plot_bgcolor='rgb(17, 17, 17)',  
-    paper_bgcolor='rgb(17, 17, 17)', 
-    font_color='white',  
-    title='Distribution of C. Types',
-)
-fig.update_traces(marker=dict(colors=['red']))
-fig.show()'''
-
-
-top_countries = netflix_data['country'].value_counts().head(10)
-
-'''fig = px.treemap(names=top_countries.index, parents=["" for _ in top_countries.index], values=top_countries.values)
-
-fig.update_layout(
-    plot_bgcolor='rgb(17, 17, 17)',  
-    paper_bgcolor='rgb(17, 17, 17)', 
-    font_color='white',  
-    title='Top Countries with Highest Number of Movies',
-)
-fig.show()
-'''
-
-'''country_movie_counts = netflix_data['country'].value_counts()
-
-data = pd.DataFrame({'Country': country_movie_counts.index, 'Movie Count': country_movie_counts.values})
-
-fig = px.choropleth(data_frame=data, locations='Country', locationmode='country names',
-                    color='Movie Count', title='Number of Movies Released By Country',
-                    color_continuous_scale='Reds', range_color=(0, data['Movie Count'].max()),
-                    labels={'Movie Count': 'Number of Movies'})
-
-fig.update_layout(
-    plot_bgcolor='rgb(17, 17, 17)',  
-    paper_bgcolor='rgb(17, 17, 17)', 
-    font_color='white' 
-)
-fig.show()'''
-
-
-ratings       = list(netflix_data['rating'].value_counts().index)
-rating_counts = list(netflix_data['rating'].value_counts().values)
-
-'''fig = go.Figure(data=[go.Bar(
-    x=ratings,
-    y=rating_counts,
-    marker_color='#E50914'
-)])
-
-fig.update_layout(
-    title='Movie Ratings Distribution',
-    xaxis_title='Rating',
-    yaxis_title='Count',
-    plot_bgcolor='rgba(0, 0, 0, 0)',
-    paper_bgcolor='rgba(0, 0, 0, 0.7)',
-    font=dict(
-        color='white'
-    )
-)
-
-fig.show()'''
-
-
-ratings       = list(netflix_data['duration'].value_counts().index)
-rating_counts = list(netflix_data['duration'].value_counts().values)
-
-'''fig = go.Figure(data=[go.Bar(
-    x=ratings,
-    y=rating_counts,
-    marker_color='#E50914'
-)])
-
-fig.update_layout(
-    title='Movie Durations Distribution',
-    xaxis_title='Rating',
-    yaxis_title='Count',
-    plot_bgcolor='rgba(0, 0, 0, 0)',
-    paper_bgcolor='rgba(0, 0, 0, 0.7)',
-    font=dict(
-        color='white'
-    )
-)
-
-fig.show()
-'''
-
-titles = netflix_data['title'].values
-
-text = ' '.join(titles)
-
-wordcloud = WordCloud(background_color='black', colormap='Reds').generate(text)
-
-'''plt.figure(figsize=(10, 6))
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis('off')
-plt.title('Most Common Words in Netflix Titles', color='white')
-plt.show()'''
-
-
-titles = netflix_data['description'].values
-
-text = ' '.join(titles)
-
-wordcloud = WordCloud(background_color='black', colormap='Reds').generate(text)
-
-'''plt.figure(figsize=(10, 6))
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis('off')
-plt.title('Most Common Words in Netflix Descriptions', color='white')
-'''
-
-
-titles = netflix_data['listed_in'].values
-
-text = ' '.join(titles)
-
-wordcloud = WordCloud(background_color='black', colormap='Reds').generate(text)
-
-'''plt.figure(figsize=(10, 6))
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis('off')
-plt.title('Most Common Words in Netflix Descriptions', color='white')'''
-
-
+# Feature engineering
 new_data = netflix_data[['title', 'type', 'director', 'cast', 'rating', 'listed_in', 'description']]
 new_data.set_index('title', inplace=True)
 
-
 class TextCleaner:
-    def separate_text(self, texts):
-        unique_texts = set()
-        for text in texts.split(','):
-            unique_texts.add(text.strip().lower())
-        return ' '.join(unique_texts)
+    def clean_text(self, text):
+        text = ' '.join(set(text.split(','))).lower()  # Remove duplicates (comma-separated) and lowercase
+        text = text.replace(' ', '')  # Remove spaces
+        text = text.translate(str.maketrans('', '', string.punctuation))  # Remove punctuation
+        return text
 
-    def remove_space(self, texts):
-        return texts.replace(' ', '').lower()
-
-    def remove_punc(self, texts):
-        texts = texts.lower()
-        texts = texts.translate(str.maketrans('', '', string.punctuation))
-        return ' '.join(texts.split())
-
-    def clean_text(self, texts):
-        texts = self.separate_text(texts)
-        texts = self.remove_space(texts)
-        texts = self.remove_punc(texts)
-        return texts
-    
 cleaner = TextCleaner()
+new_data['type'] = new_data['type'].apply(cleaner.clean_text)
+new_data['director'] = new_data['director'].apply(cleaner.clean_text)
+new_data['cast'] = new_data['cast'].apply(cleaner.clean_text)
+new_data['rating'] = new_data['rating'].apply(cleaner.clean_text)
+new_data['listed_in'] = new_data['listed_in'].apply(cleaner.clean_text)
+new_data['description'] = new_data['description'].apply(cleaner.clean_text)
 
-new_data['type']        = new_data['type'].apply(cleaner.remove_space)
-new_data['director']    = new_data['director'].apply(cleaner.separate_text)
-new_data['cast']        = new_data['cast'].apply(cleaner.separate_text)
-new_data['rating']      = new_data['rating'].apply(cleaner.remove_space)
-new_data['listed_in']   = new_data['listed_in'].apply(cleaner.separate_text)
-new_data['description'] = new_data['description'].apply(cleaner.remove_punc)
+# Combine features into a single string for each movie
+new_data['BoW'] = new_data[['type', 'director', 'cast', 'rating', 'listed_in', 'description']].apply(lambda row: ' '.join(row), axis=1)
 
-
-new_data['BoW'] = new_data.apply(lambda row: ' '.join(row.dropna().values), axis=1)
-new_data.drop(new_data.columns[:-1], axis=1, inplace=True)
-
-
+# TF-IDF and Cosine Similarity
 tfid = TfidfVectorizer()
 tfid_matrix = tfid.fit_transform(new_data['BoW'])
-
 cosine_sim = cosine_similarity(tfid_matrix, tfid_matrix)
-
 
 np.save('tfidf_matrix.npy', tfid_matrix)
 np.save('cosine_sim_matrix.npy', cosine_sim)
@@ -215,9 +49,7 @@ with open('tfidf_vectorizer.pkl', 'wb') as f:
     pickle.dump(tfid, f)
 
 final_data = netflix_data[['title', 'type']]
-
-final_data.to_csv('movie_data.csv',index=False)
-
+final_data.to_csv('movie_data.csv', index=False)
 
 class FlixHub:
     def __init__(self, df, cosine_sim):
@@ -226,31 +58,33 @@ class FlixHub:
     
     def recommendation(self, title, total_result=5, threshold=0.5):
         idx = self.find_id(title)
-        self.df['similarity'] = self.cosine_sim[idx]
-        sort_df = self.df.sort_values(by='similarity', ascending=False)[1:total_result+1]
+        if idx == -1:
+            return "Movie not found."
         
-        movies = sort_df['title'][sort_df['type'] == 'Movie']
-        tv_shows = sort_df['title'][sort_df['type'] == 'TV Show']
+        # Calculate similarities and filter based on the threshold
+        similarities = self.cosine_sim[idx]
+        self.df['similarity'] = similarities
+        sorted_df = self.df.sort_values(by='similarity', ascending=False)
         
-        similar_movies = []
-        similar_tv_shows = []
+        # Filter out movies and TV shows below the threshold
+        sorted_df = sorted_df[sorted_df['similarity'] >= threshold]
         
-        for i, movie in enumerate(movies):
-            similar_movies.append('{}. {}'.format(i+1, movie))
+        # Get movies and TV shows separately
+        movies = sorted_df[sorted_df['type'] == 'Movie']['title'].head(total_result)
+        tv_shows = sorted_df[sorted_df['type'] == 'TV Show']['title'].head(total_result)
         
-        for i, tv_show in enumerate(tv_shows):
-            similar_tv_shows.append('{}. {}'.format(i+1, tv_show))
+        # Format the results
+        similar_movies = [f"{i+1}. {movie}" for i, movie in enumerate(movies)]
+        similar_tv_shows = [f"{i+1}. {tv_show}" for i, tv_show in enumerate(tv_shows)]
         
         return similar_movies, similar_tv_shows
 
     def find_id(self, name):
-        for index, string in enumerate(self.df['title']):
-            if re.search(name, string):
-                return index
-        return -1
+        # Search for movie by title
+        idx = self.df[self.df['title'].str.contains(name, case=False, na=False)].index
+        return idx[0] if len(idx) > 0 else -1
 
-
-
+# Example usage:
 flix_hub = FlixHub(final_data, cosine_sim)
 movies, tv_shows = flix_hub.recommendation('13 Reasons Why', total_result=10, threshold=0.5)
 
@@ -258,6 +92,6 @@ print('Similar Movie(s) list:')
 for movie in movies:
     print(movie)
 
-print('\nSimilar TV_show(s) list:')
+print('\nSimilar TV Show(s) list:')
 for tv_show in tv_shows:
     print(tv_show)
